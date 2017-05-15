4a832a111c26a30360506d17a0dfd645bea047d8
Migelo <miha@filetki.si>
2017-05-15 20:19:34 +0200

    pep8

--- generateAtmospheres.py
+++ generateAtmospheres.py
@@ -2,17 +2,16 @@
 import numpy as np
 
 temperatures = np.logspace(3.3, 5.3, 57)
-    
 pressure = np.logspace(-4, 8, 25)
 
 for temp in temperatures:
     i = 0
-    table=[]
+    table = []
     for p in pressure:
-        concentration=p/(1.38*10**-16*temp)
-	if ( concentration < 2.e19 ):
-	    table.append([len(pressure)*100-i*100, temp, 0, concentration, 1])
-            i += 1
-	else:
-	    break
-    np.savetxt('T' + str(temp).split('.')[0] + '.atm', table, fmt = '%.2f %.2f  %.5e  %.5e %.5f')
+        concentration = p / (1.38 * 10**-16 * temp)
+        if concentration < 2.e19:
+                table.append([len(pressure) * 100 - i * 100, temp, 0, concentration, 1])
+                i += 1
+        else:
+            break
+    np.savetxt('T' + str(temp).split('.')[0] + '.atm', table, fmt='%.2f %.2f  %.5e  %.5e %.5f')
