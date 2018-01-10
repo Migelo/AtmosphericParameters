ddafc4a650f8f305a7d67cc7e782111f707bf163
Migelo <miha@filetki.si>
2018-01-10 16:30:07 +0100

    PEP8

--- divide.py
+++ divide.py
@@ -1,21 +1,18 @@
 import numpy as np
 import argparse
-import os
 
 parser = argparse.ArgumentParser(description='Calculate contrast. (spectra1 - spectra2) / (spectra3 - spectra4)')
 parser.add_argument('spectra1', type=str, help='First spectra.')
 parser.add_argument('spectra2', type=str, help='Second spectra.')
 parser.add_argument('spectra3', type=str, help='Third spectra.')
 parser.add_argument('spectra4', type=str, help='Fourth spectra.')
-
 args = parser.parse_args()
-#set the necessary parameters for parsing
 
 spectra1 = np.loadtxt(args.spectra1)
 spectra2 = np.loadtxt(args.spectra2)
 spectra3 = np.loadtxt(args.spectra3)
 spectra4 = np.loadtxt(args.spectra4)
 
-result = (spectra1[:,-1] - spectra2[:,-1])/(spectra3[:,-1] - spectra4[:,-1])
+result = (spectra1[:, -1] - spectra2[:, -1]) / (spectra3[:, -1] - spectra4[:, -1])
 
-np.savetxt(args.spectra1.split("/")[-1] + "Contrast",np.c_[spectra1[:,0],result])
+np.savetxt(args.spectra1.split("/")[-1] + "Contrast", np.c_[spectra1[:,0],result])
