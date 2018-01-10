7e0c2cf92c8e7533598aa351c46fa90fb3654ff8
Migelo <miha@filetki.si>
2018-01-10 16:32:03 +0100

    PEP8

--- plotData.py
+++ plotData.py
@@ -3,24 +3,25 @@ import numpy as np
 import glob
 import time
 
-binToPlot = [0,1,2]
-subBinsToPlotDefault = [0,4,9]
+binToPlot = [0, 1, 2]
+subBinsToPlotDefault = [0, 4, 9]
 Nh = 16
-t0=time.clock()
+t0 = time.clock()
 floatFormat = '%.7e'
 
 for binn in binToPlot:
     subBinsToPlot = []
-    for item in subBinsToPlotDefault: subBinsToPlot.append(item + binn*10)
+    for item in subBinsToPlotDefault:
+        subBinsToPlot.append(item + binn * 10)
     print subBinsToPlot
     for subBins in subBinsToPlot:
         plotData = []
         for folder in sorted(glob.glob('subBins/T*')):
-            for segment in sorted(glob.glob(folder + '/' + str(Nh) + '.r10plot')): # returns only 1 file
+            for segment in sorted(glob.glob(folder + '/' + str(Nh) + '.r10plot')):  # returns only 1 file
                 print segment
-                a=np.loadtxt(segment)[subBins]
+                a = np.loadtxt(segment)[subBins]
                 print a
                 plotData.append([int(segment.split('/')[-2].split('T')[-1]), a[2]])
         plotData = sorted(plotData)
-        np.savetxt(str(binn+1) + 'bin_' + str(subBins-(binn*10)+1) + 'sub', plotData, fmt=floatFormat) 
-print('Finished in ', time.clock()-t0)
+        np.savetxt(str(binn + 1) + 'bin_' + str(subBins - (binn * 10) + 1) + 'sub', plotData, fmt=floatFormat)
+print('Finished in ', time.clock() - t0)
